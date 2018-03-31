4f77aed1c85d46036efe2c23e3ef1fb994b6047b
Arthur Pachachura <pachachura.arthur@gmail.com>
2018-03-30 21:13:30 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,31 +1,31 @@
-const parseDateNumber = function(a) {
-  if (a.toString().length == 2) return a.toString();
-  else return "0" + a.toString();
-}
+// const parseDateNumber = function(a) {
+//   if (a.toString().length == 2) return a.toString();
+//   else return "0" + a.toString();
+// }
 
-$(document).ready(function(){
-  var eventTime = moment("2018-03-31 11:00:00.000-06:00");
+// $(document).ready(function(){
+//   var eventTime = moment("2018-03-31 11:00:00.000-06:00");
 
-  const onUpdate = function() {
-    var remain = eventTime.diff(moment());
+//   const onUpdate = function() {
+//     var remain = eventTime.diff(moment());
 
-    var days = Math.floor(remain / (1000 * 60 * 60 * 24));
-    remain -= days * (1000 * 60 * 60 * 24);
+//     var days = Math.floor(remain / (1000 * 60 * 60 * 24));
+//     remain -= days * (1000 * 60 * 60 * 24);
 
-    var hrs = Math.floor(remain / (1000 * 60 * 60));
-    remain -= hrs * (1000 * 60 * 60);
+//     var hrs = Math.floor(remain / (1000 * 60 * 60));
+//     remain -= hrs * (1000 * 60 * 60);
 
-    var mins = Math.floor(remain / (1000 * 60));
-    remain -= mins * (1000 * 60);
+//     var mins = Math.floor(remain / (1000 * 60));
+//     remain -= mins * (1000 * 60);
 
-    var secs = Math.floor(remain / (1000));
+//     var secs = Math.floor(remain / (1000));
 
-    $("#secs").text(parseDateNumber(secs));
-    $("#mins").text(parseDateNumber(mins));
-    $("#hours").text(parseDateNumber(hrs));
-    $("#days").text(parseDateNumber(days));
-  }
+//     $("#secs").text(parseDateNumber(secs));
+//     $("#mins").text(parseDateNumber(mins));
+//     $("#hours").text(parseDateNumber(hrs));
+//     $("#days").text(parseDateNumber(days));
+//   }
 
-  onUpdate();
-  setInterval(onUpdate, 1000);
-});
+//   onUpdate();
+//   setInterval(onUpdate, 1000);
+// });
